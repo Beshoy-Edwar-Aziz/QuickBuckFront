fcf10dde569fcc0ec5c01f96ec1462d550c8de7d
Beshoy Edwar <beshoyedwar8@gmail.com>
2024-06-24 01:32:57 +0300

    commit -a

--- src/app/Components/footer/footer.component.ts
+++ src/app/Components/footer/footer.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AuthServiceService } from '../../Services/auth-service.service';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  constructor(private _authService:AuthServiceService){}
+  Token:any = this._authService.Token;
 }
